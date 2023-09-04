1c3d0a0d8fde08fe14c7aa821e359b1e1f8bda66
Ante-237 <akumawahanye@gmail.com>
2023-09-04 22:41:02 +0200

    manual directory creation

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,4 +1,13 @@
 #!/usr/bin/env bash
 
 PROJECT_DIR="0-new_project"
-dotnet new console -n $PROJECT_DIR
+
+if [ -d "$PROJECT_DIR" ]; then
+	rmdir $PROJECT_DIR
+	exit 1
+fi
+
+mkdir "$PROJECT_DIR"
+cd "$PROJECT_DIR"
+
+dotnet new console
