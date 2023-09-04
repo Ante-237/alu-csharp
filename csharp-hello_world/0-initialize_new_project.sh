6c26c1a97b57a3c22d5f369ae5d2e0a892790f28
Ante-237 <akumawahanye@gmail.com>
2023-09-04 22:43:36 +0200

    old creation

--- csharp-hello_world/0-initialize_new_project.sh
+++ csharp-hello_world/0-initialize_new_project.sh
@@ -1,13 +1,4 @@
 #!/usr/bin/env bash
 
 PROJECT_DIR="0-new_project"
-
-if [ -d "$PROJECT_DIR" ]; then
-	rmdir $PROJECT_DIR
-	exit 1
-fi
-
-mkdir "$PROJECT_DIR"
-cd "$PROJECT_DIR"
-
-dotnet new console
+dotnet new console -n "$PROJECT_DIR"
