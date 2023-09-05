6a607dddaf424c2ef74ba9d0f9c2d7e5ff96fe98
Ante-237 <akumawahanye@gmail.com>
2023-09-06 01:13:57 +0200

    copy cut paste

--- csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
+++ csharp-hello_world/10-copy_cut_paste/10-copy_cut_paste.cs
@@ -5,10 +5,8 @@ class Program
         static void Main(string[] args)
         {
 		string word = "Holberton";
-		string wordTwo  = word;
-        string wordThree = word;
 		Console.WriteLine("First 3 letters: {0}", word.Substring(0,3));
-		Console.WriteLine("Last 2 letters: {0}", wordTwo.Substring(7,2));
-		Console.WriteLine("Middle word: {0}", wordThree.Substring(1, 7));
+		Console.WriteLine("Last 2 letters: {0}", word.Substring(7,2));
+		Console.WriteLine("Middle word: {0}", word.Substring(1, 7));
         }
 }
