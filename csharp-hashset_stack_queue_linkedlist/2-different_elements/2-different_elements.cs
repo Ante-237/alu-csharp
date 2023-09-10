76e3f4cf801c023c305a93a12d4831c66e8c3947
Ante-237 <akumawahanye@gmail.com>
2023-09-10 03:01:45 +0200

    hashset checking

---
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+
+class List{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2){
+        HashSet<int> HashOne = new HashSet<int>(list1);
+        HashSet<int> HashTwo = new HashSet<int>(list2);
+
+        HashOne.SymmetricExceptWith(HashTwo);
+
+        return new List<int>(HashOne);
+    }
+}
