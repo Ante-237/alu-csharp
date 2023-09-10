b82179d19c59697ec22498c9c867dd47f2ddbefa
Ante-237 <akumawahanye@gmail.com>
2023-09-10 03:06:23 +0200

    only difference, arrray order

--- csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
+++ csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -1,11 +1,28 @@
 ﻿using System.Collections;
 
 class List{
+
+     static void Main(string[] args)
+    {
+        List<int> myList1 = new List<int>() {1, 2, 3, 4, 5, 6};
+        List<int> myList2 = new List<int>() {0, 2, 4, 6, 7, 8};
+        List<int> different;
+
+        different = List.DifferentElements(myList1, myList2);
+
+        foreach (int i in different)
+            Console.WriteLine(i);
+    }
     public static List<int> DifferentElements(List<int> list1, List<int> list2){
         HashSet<int> HashOne = new HashSet<int>(list1);
         HashSet<int> HashTwo = new HashSet<int>(list2);
 
         HashOne.SymmetricExceptWith(HashTwo);
+        int[] arrInt;
+        arrInt = HashOne.ToArray();
+        Array.Sort(arrInt);
+        HashOne.Clear();
+        HashOne.UnionWith(arrInt);
 
         return new List<int>(HashOne);
     }
