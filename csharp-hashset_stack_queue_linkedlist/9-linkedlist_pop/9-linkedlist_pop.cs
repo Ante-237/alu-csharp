695189c31c581e72cc1bf6b59283d07b0b55bfa7
Ante-237 <akumawahanye@gmail.com>
2023-09-11 00:17:33 +0200

    pop update

--- csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
+++ csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -1,21 +1,6 @@
 ﻿using System.Collections.Generic;
 
 class LList{
-
-    static void Main(string[] args)
-    {
-        LinkedList<int> myLList = new LinkedList<int>();
-
-        myLList.AddLast(11);
-        myLList.AddLast(3);
-        myLList.AddLast(-9);
-        myLList.AddLast(47);
-        myLList.AddLast(0);
-        myLList.AddLast(-9);
-
-        Console.WriteLine(LList.Pop(myLList));
-    }
-
     public static int Pop(LinkedList<int> myList){
         if(myList.Count == 0){
             return 0;
