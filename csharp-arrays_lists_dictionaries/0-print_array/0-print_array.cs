7188cbaa5d9ce35fcdc33c62d395a86e12494e21
Ante-237 <akumawahanye@gmail.com>
2023-09-19 10:40:40 +0200

    changes

--- csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
+++ csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -1,19 +1,39 @@
 ﻿using System.Collections;
 
+
+        int[] newArray;
+
+        newArray = Array.CreatePrint(10);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(16);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(0);
+        Console.WriteLine("Array Length: " + newArray.Length);
+        Console.WriteLine("----------------");
+        newArray = Array.CreatePrint(-10);
+
+
+
 class Array{
+
     public static int[] CreatePrint(int size){
-        int[] NumberList = new int[size];
+
         if(size == 0){
             Console.WriteLine();
-            return NumberList;
+            return new int[0];
         }else if(size < 0){
             Console.WriteLine("Size cannot be negative");
             return null;
         }
 
+        int[] NumberList = new int[size];
         for(int i = 0; i < size; i ++){
             NumberList[i] = i;
+            Console.Write($"{i} ");
         }
+        Console.WriteLine();
 
         return NumberList;
     }
