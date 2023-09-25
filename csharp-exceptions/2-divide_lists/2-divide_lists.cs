76a020e764951a9e13f2875a57a8508483bc2c70
Ante-237 <akumawahanye@gmail.com>
2023-09-25 11:35:34 +0200

    divide lists

--- csharp-exceptions/2-divide_lists/2-divide_lists.cs
+++ csharp-exceptions/2-divide_lists/2-divide_lists.cs
@@ -2,19 +2,6 @@
 using System;
 
 class List{
-
-     public static void Main(string[] args)
-    {
-        List<int> list1 = new List<int>() {1, 20, 16, 15, 54};
-        List<int> list2 = new List<int>() {1, 0, 2, 3};
-        List<int> result;
-
-        result = Divide(list1, list2, 5);
-
-        foreach (int i in result)
-            Console.WriteLine(i);
-    }
-
     public static List<int> Divide(List<int> list1, List<int> list2, int listLength){
         List<int> result  = new List<int>();
         int TempHold = 0;
