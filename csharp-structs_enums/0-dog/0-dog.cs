357dba585df7bfb8d35a72d6dd6cef779c336f1c
Ante-237 <akumawahanye@gmail.com>
2023-10-01 18:09:29 +0200

    they are good dogs

---
+++ csharp-structs_enums/0-dog/0-dog.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public enum Rating{
+    Good,
+    Great, 
+    Excellent,
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rating rating;
+
+        rating = Rating.Great;
+
+        Console.WriteLine("Score: " + rating);
+        Console.WriteLine("Score: " + (int)rating);
+    }
+}
