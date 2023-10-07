1861e69e000c910688e1796d99e2fd824d2ba8a2
Ante-237 <akumawahanye@gmail.com>
2023-10-07 21:02:54 +0200

    string formating

--- csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		double percent = .7553;
 		double currency = 98765.4321;
-		Console.WriteLine($"Percent: {percent.ToString("P")}");
+		Console.WriteLine($"Percent: {(percent * 100):N}%");
         Console.WriteLine($"Currency: {string.Format("{0:C2}", currency)}");
     }
 }
