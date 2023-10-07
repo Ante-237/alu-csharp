291e8151625fecf2b4100e00b35ed398ced0f794
Ante-237 <akumawahanye@gmail.com>
2023-10-07 23:10:24 +0200

    changes

--- csharp-hello_world/7-concat/7-concat.cs
+++ csharp-hello_world/7-concat/7-concat.cs
@@ -6,7 +6,7 @@ class Program
 	{
 		string str1 = "Holberton";
 		string str2 = " School";
-	    string str3 = str1 + str2;
-		Console.WriteLine($"Welcome to {str3}!");
+	    str1 = str1 + str2;
+		Console.WriteLine($"Welcome to {str1}!");
 	}
 }
