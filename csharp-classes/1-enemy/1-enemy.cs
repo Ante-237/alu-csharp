c0f8701c19af3446dde4c0f67a718e571247bd7e
Ante-237 <akumawahanye@gmail.com>
2023-10-08 13:12:29 +0200

    healthy competition

---
+++ csharp-classes/1-enemy/1-enemy.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Enemies{
+   
+    /// <summary>
+    /// Zombie is a public class 
+    /// </summary>
+    /// <param name="health"> Health Field used by Zombi </param>
+    public class Zombie{
+
+        public int health;
+
+        public Zombie(){
+            health = 0;
+        }
+
+    }
+}
+
