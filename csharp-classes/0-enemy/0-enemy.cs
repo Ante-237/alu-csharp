65ba714b57a78281a56aa4399e2fe77cbccdc0d0
Ante-237 <akumawahanye@gmail.com>
2023-10-08 12:59:51 +0200

    remove template test code

--- csharp-classes/0-enemy/0-enemy.cs
+++ csharp-classes/0-enemy/0-enemy.cs
@@ -1,16 +1,5 @@
 ﻿using System;
 
-class Program
-{
-    static void Main(string[] args)
-    {
-        Enemies.Zombie newObject = new Enemies.Zombie();
-
-        Console.WriteLine("newObject is of type {0}", newObject.GetType());
-        Console.WriteLine("String representation: {0}", newObject.ToString());
-    }
-}
-
 namespace Enemies{
     /// <summary>
     ///  zombie is an empty class
