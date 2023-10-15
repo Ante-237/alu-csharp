2b8d5a0dae8a92be188c858cf4f97a5a37ecf197
Ante-237 <akumawahanye@gmail.com>
2023-10-15 23:59:05 +0200

    more setup

---
+++ csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,22 @@
+using MyMath;
+using NUnit.Framework;
+
+
+namespace MyMath.Tests{
+public class Tests
+{
+    [SetUp]
+    public void Setup()
+    {
+       
+    }
+
+    [Test]
+    public void AdditionOperation()
+    {
+        Assert.IsTrue()
+    }
+}
+}
+
+
