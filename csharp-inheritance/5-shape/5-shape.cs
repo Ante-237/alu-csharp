4b258d0f23b49c2b1b5aef9b62807ad49562100c
Ante-237 <akumawahanye@gmail.com>
2023-11-03 11:20:41 +0200

    documentation addition

--- csharp-inheritance/5-shape/5-shape.cs
+++ csharp-inheritance/5-shape/5-shape.cs
@@ -3,6 +3,11 @@
 
 public class Shape{
 
+/// <summary>
+/// Emtpy area calculation
+/// </summary>
+/// <returns></returns>
+/// <exception cref="NotImplementedException"></exception>
     public virtual int Area(){
         throw new NotImplementedException("Area() is not implemented");
     }
