19699956f1d6dcd4e2eca688dd3cd549e989dacd
Ante-237 <akumawahanye@gmail.com>
2023-11-03 11:49:45 +0200

    Rectangle

--- csharp-inheritance/6-shape/6-shape.cs
+++ csharp-inheritance/6-shape/6-shape.cs
@@ -1,6 +1,39 @@
 ﻿using System;
 
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Rectangle aRect = new Rectangle();
+        aRect.Width = 10;
+        aRect.Height = 2;
+
+        if (typeof(Rectangle).IsSubclassOf(typeof(Shape)))
+            Console.WriteLine("Rectangle is a subclass of Shape");
+        else
+            Console.WriteLine("Rectangle is a NOT subclass of Shape");
+
+        Console.WriteLine("Width: {0}", aRect.Width);
+        Console.WriteLine("Height: {0}", aRect.Height);
+
+        try
+        {
+            Console.WriteLine("Area: {0}", aRect.Area());
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e);
+        }
+    }
+}
+
 public class Shape{
+
+}
+
+
+public class Rectangle : Shape{
 
     private int width;
     private int height;
@@ -41,9 +74,4 @@ public class Shape{
     public virtual int Area(){
         throw new NotImplementedException("Area() is not implemented");
     }
-}
-
-
-public class Rectangle : Shape{
-
 }
