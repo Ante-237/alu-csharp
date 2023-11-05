ea385b26e4e07528843784719bc234edda3373b0
Ante-237 <akumawahanye@gmail.com>
2023-11-05 14:57:59 +0200

    matrix multiplication code

---
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class MatrixMath{
+
+    public static double[,] Multiply(double[,] matrix1, double[,] matrix2){
+       
+        if(matrix1.GetLength(0) == matrix2.GetLength(1)){
+            for(int i = 0; i < matrix1.GetLength(0); i++){
+                for(int j = 0; j < matrix1.GetLength(1); j++){
+                   matrix1[i,j] =   matrix1[i, j] + matrix2[j, i];
+                }
+            }
+            return matrix1;
+        }
+
+        return new double[,]{{-1}};
+    }
+}
