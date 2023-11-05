c7d779cbbeb33b21a5cfabd5d14a518331d11ef2
Ante-237 <akumawahanye@gmail.com>
2023-11-05 11:49:19 +0200

    pythagoras

---
+++ csharp-linear_algebra/Calculations/Calculations.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program{
+
+    public static void Main(string[] args){
+
+        Console.WriteLine("working class");
+        Console.WriteLine(" Length  :" + Pythagoras(64, 121));
+    }
+
+    public static float Pythagoras(int a, int b){
+
+        int aS = (int) Math.Pow(a, 2);
+        int bS = (int) Math.Pow(b, 2);
+
+        int result = (int) Math.Sqrt(aS + bS);
+
+        return result;
+    }
+}
