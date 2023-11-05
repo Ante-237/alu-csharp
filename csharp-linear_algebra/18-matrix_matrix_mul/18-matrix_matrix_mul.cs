dd26a1286ce590803744d81281fe85814dd64a85
Ante-237 <akumawahanye@gmail.com>
2023-11-05 15:31:51 +0200

    matrix mul

--- csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
+++ csharp-linear_algebra/18-matrix_matrix_mul/18-matrix_matrix_mul.cs
@@ -3,13 +3,25 @@
 class MatrixMath{
 
     public static double[,] Multiply(double[,] matrix1, double[,] matrix2){
+
+        int rowA = matrix1.GetLength(0);
+        int colA = matrix1.GetLength(1);
+        int rowB = matrix2.GetLength(0);
+        int colB = matrix2.GetLength(1);
+
+        double[,] result = new double[rowA, colB];
        
-        if(matrix1.GetLength(0) == matrix2.GetLength(1)){
-            for(int i = 0; i < matrix1.GetLength(0); i++){
-                for(int j = 0; j < matrix1.GetLength(1); j++){
-                   matrix1[i,j] =   matrix1[i, j] + matrix2[j, i];
+        if(colsA == rowsB){
+            for(int i = 0; i < rowA; i++){
+
+                for(int j = 0; j < colB; j++){
+                  
+                  for(int u = 0; u < colA; u++){
+                        resultMatrix[i,j] += matrix1[i, u] * matrix2[u, j];
+                  }
                 }
             }
+
             return matrix1;
         }
 
