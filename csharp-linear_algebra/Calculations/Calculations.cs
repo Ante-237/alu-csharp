00caeb805f507b2fcc6e184fc07e1c684567eb6d
Ante-237 <akumawahanye@gmail.com>
2023-11-05 12:43:13 +0200

    vector addition 2D

--- csharp-linear_algebra/Calculations/Calculations.cs
+++ csharp-linear_algebra/Calculations/Calculations.cs
@@ -4,7 +4,7 @@ class Program{
 
     public static void Main(string[] args){
 
-        Console.WriteLine("Magnitude : " + Pythagoras3D(7,-3, -9));
+        Console.WriteLine("VectorSum : " + SumVectors(8, -11, -4, 9)[0] + " : "  + SumVectors(8,-11,-4,9)[1]);
     }
 
     // calculating pythagoras length
@@ -20,6 +20,14 @@ class Program{
 
     public static double Pythagoras3D(double x, double y, double z){
         double result = Math.Round(Math.Sqrt( Math.Pow(x,2) + Math.Pow(y, 2) + Math.Pow(z, 2)), 2);
+        return result;
+    }
+
+    public static double[] SumVectors(double x1, double y1, double x2, double y2){
+
+        double[] result = new double[2];
+        result[0] = x1 + x2;
+        result[1] = y1 + y2;
         return result;
     }
 
