452ecf2308eba0f8052c432ba3c45fc519477abd
Ante-237 <akumawahanye@gmail.com>
2023-11-05 14:24:26 +0200

    matrix multiplication code

---
+++ csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class MatrixMath{
+
+    public static double[,] MultiplyScalar(double[,] matrix, double scalar){
+        if(matrix.GetLength(1) == 2 || matrix.GetLength(1) == 3){
+            for(int i = 0; i < matrix.GetLength(0); i++){
+                for(int j = 0; j < maxtrix.GetLength(1); j++){
+                    matrix[i,j] = scalar * matrix[i,j];
+                }
+            }
+            return matrix;
+        }
+
+        return new double[]{{-1}};
+    }
+}
