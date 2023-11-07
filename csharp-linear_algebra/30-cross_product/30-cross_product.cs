b17d7cb76b13c9b24b785c99593bf2ff10f3a37a
Ante-237 <akumawahanye@gmail.com>
2023-11-07 22:30:18 +0200

    inverse

--- csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -6,9 +6,10 @@ class VectorMath{
 
         if(vector1.Length == 3 && vector2.Length == 3){
 
-            double x =  (vector1[1] * vector2[2]) - (vector2[1] * vector1[2]);
+            double x =  ((vector1[1] * vector2[2]) - (vector2[1] * vector1[2]));
             double y = -((vector1[0] * vector2[2]) - (vector2[0] * vector1[2]));
             double z =  ((vector1[0] * vector2[1]) - (vector1[1] * vector2[0]));
+            
 
             return new double[]{ x, y, z};
         }
