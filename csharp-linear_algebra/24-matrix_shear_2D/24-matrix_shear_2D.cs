ebc4190a79de8db4952174f092374523195e57e0
Ante-237 <akumawahanye@gmail.com>
2023-11-17 23:25:57 +0200

    shearing

--- csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
+++ csharp-linear_algebra/24-matrix_shear_2D/24-matrix_shear_2D.cs
@@ -6,29 +6,32 @@ public class MatrixMath
     public static double[,] Shear2D(double[,] matrix, char direction, double factor){
         int rows = matrix.GetLength(0);
         int cols = matrix.GetLength(1);
-        double[,] mMatrix = new double[,]{{-1}};
         double[,] result = new double[rows, cols];
+        double[,] mMatrix = new double[rows, cols];
 
         if(rows != 2 || cols != 2){
+            Console.WriteLine(" Is a 2 by 2 matrix");
             return new double[,]{ {-1}};
         }
 
 
-        if(direction != 'x'  || direction != 'y'){
+        if(direction != 'x' && direction != 'y'){
+            Console.WriteLine(" Is not x or y");
             return new double[,] { { -1}};
         }
         
 
         // create matrix 
         if(direction == 'x'){
-            mMatrix = new double[,]{ 
+            Console.WriteLine("X running");
+         mMatrix = new double[,]{ 
                 { 1, 0},
                 { factor, 1}
         };
         }
 
         if(direction == 'y'){
-            mMatrix = new double[,]{
+             mMatrix = new double[,]{
                 { 1, factor},
                 { 0, 1}
             };
@@ -37,7 +40,7 @@ public class MatrixMath
         for(int i = 0; i < rows; i++){
 
                 for(int j = 0; j < cols; j++){
-                  
+                
                   for(int u = 0; u < 2; u++){
                         result[i,j] += Math.Round(matrix[i, u] * mMatrix[u, j], 2);
                 }
@@ -47,72 +50,26 @@ public class MatrixMath
         return result;
 
     }
-
-    /*
-    // Applies a shear along the X-axis to a 2D matrix of points.
-    public static double[,] ShearX(double[,] matrix, double shx)
-    {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-        double[,] result = new double[rows, cols];
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                double x = matrix[i, j];
-                double y = i; // Assuming each row corresponds to a Y coordinate
-                // Applying shear transformation
-                result[i, j] = x + shx * y;
-            }
-        }
-
-        return result;
-
-
-    }
-
-    // Applies a shear along the Y-axis to a 2D matrix of points.
-    public static double[,] ShearY(double[,] matrix, double shy)
-    {
-        int rows = matrix.GetLength(0);
-        int cols = matrix.GetLength(1);
-        double[,] result = new double[rows, cols];
-
-        for (int i = 0; i < rows; i++)
-        {
-            for (int j = 0; j < cols; j++)
-            {
-                double x = j; // Assuming each column corresponds to an X coordinate
-                double y = matrix[i, j];
-                // Applying shear transformation
-                result[i, j] = shy * x + y;
-            }
-        }
-
-        return result;
-    }*/
 }
 
 
 /*
 class Program
 {
-    static void Main()
+    public static void Main(string[] arg)
     {
         double[,] matrix = {
             { 1, 2 },
             { 3, 4 }
         };
 
-        double shearFactorX = 2; // Shear along X-axis
-        double shearFactorY = 2; // Shear along Y-axis
+        double shearFactorX = 2.2; // Shear along X-axis
+        // Shear along Y-axis
 
-        double[,] shearedMatrixX = MatrixTransformation.ShearX(matrix, shearFactorX);
-        double[,] shearedMatrixY = MatrixTransformation.ShearY(matrix, shearFactorY);
+        double[,] shearedMatrixX = MatrixMath.Shear2D(matrix, 'y', 2.2);
+      
 
         // Output the sheared matrix
-        Console.WriteLine("Sheared Matrix along X:");
         for (int i = 0; i < shearedMatrixX.GetLength(0); i++)
         {
             for (int j = 0; j < shearedMatrixX.GetLength(1); j++)
@@ -122,15 +79,8 @@ class Program
             Console.WriteLine();
         }
 
-        Console.WriteLine("Sheared Matrix along Y:");
-        for (int i = 0; i < shearedMatrixY.GetLength(0); i++)
-        {
-            for (int j = 0; j < shearedMatrixY.GetLength(1); j++)
-            {
-                Console.Write($"{shearedMatrixY[i, j]} ");
-            }
-            Console.WriteLine();
-        }
+     
+   
     }
 }
 */
