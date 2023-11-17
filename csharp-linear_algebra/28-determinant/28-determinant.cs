74c75cbecd457f081eb6389f1467e22c0326f8a4
Ante-237 <akumawahanye@gmail.com>
2023-11-17 22:21:04 +0200

    det

--- csharp-linear_algebra/28-determinant/28-determinant.cs
+++ csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -27,15 +27,15 @@ class MatrixMath{
 
         if (rows == 2) // 2x2 matrix
         {
-            return matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0];
+            return Math.Round(matrix[0, 0] * matrix[1, 1] - matrix[0, 1] * matrix[1, 0],2);
         }
         else if (rows == 3) // 3x3 matrix
         {
             double determinant = 0;
             determinant += matrix[0, 0] * (matrix[1, 1] * matrix[2, 2] - matrix[1, 2] * matrix[2, 1]);
             determinant -= matrix[0, 1] * (matrix[1, 0] * matrix[2, 2] - matrix[1, 2] * matrix[2, 0]);
-            determinant += Math.Abs(matrix[0, 2]) * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
-            return determinant;
+            determinant += matrix[0, 2] * (matrix[1, 0] * matrix[2, 1] - matrix[1, 1] * matrix[2, 0]);
+            return Math.Round(determinant, 2);
         }
         else
         {
